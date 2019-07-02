dfb145d45ed2ea028fe1b2b690e0db3ee64968f4
Jasper Huizing <jasper.huizing@gmail.com>
2019-07-02 15:39:08 +0200

    changed port back to 8080 and minor security config change

--- src/main/java/io/swagger/auth/AuthorizationServerConfig.java
+++ src/main/java/io/swagger/auth/AuthorizationServerConfig.java
@@ -18,6 +18,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
     @Override
     public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        security.allowFormAuthenticationForClients().checkTokenAccess("permitAll()");
         security.checkTokenAccess("isAuthenticated()");
     }
 
