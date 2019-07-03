c4498a81721366362281b092bae746957eb25f8a
Jasper Huizing <jasper.huizing@gmail.com>
2019-07-03 14:25:49 +0200

    update ibangenerator to adhere to the format

--- src/main/java/io/swagger/helper/IbanGenerator.java
+++ src/main/java/io/swagger/helper/IbanGenerator.java
@@ -11,6 +11,7 @@ public class IbanGenerator {
     public static String generateIban(BankService service){
         List<Account> accounts = service.listAllAccounts();
         Long accountNumber = Long.parseLong(accounts.get(accounts.size()-1).getIBAN().substring(9));
-        return "NL01INHO0" + ++accountNumber;
+        String incremented = String.format("%0" + 9 + "d", (accountNumber) + 1);
+        return "NL01INHO0" + incremented;
     }
 }
